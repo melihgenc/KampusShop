ddfec4d68639723e044549325bae791ecc61f9be
melihgenc <b131210307@sakarya.edu.tr>
2017-01-13 06:53:27 +0300

    Yeni activityler eklendi

---
+++ app/src/main/java/com/kampusshop/kampusshop/HesabimOff.java
@@ -0,0 +1,13 @@
+package com.kampusshop.kampusshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HesabimOff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hesabim_off);
+    }
+}
